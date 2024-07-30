526e601d02f159637dad2bc6044e4a2c2a9b8c4c
Aman Singh <aman.singh@javis.ai>
2024-07-30 21:44:10 +0530

    feat: initialize spring boot project

---
+++ src/main/java/ai/javis/menucontrol/MenucontrolApplication.java
@@ -0,0 +1,13 @@
+package ai.javis.menucontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenucontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenucontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/javis/menucontrol/MenucontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.javis.menucontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenucontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
